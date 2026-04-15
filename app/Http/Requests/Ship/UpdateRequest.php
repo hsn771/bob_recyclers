4e1f97dcf4a2c6e50e8cc0d8d081ad56455c42ac
Hasanur Rashid <hr492785@gmail.com>
2026-04-15 16:50:49 +0600

    image upload size increase

--- app/Http/Requests/Ship/UpdateRequest.php
+++ app/Http/Requests/Ship/UpdateRequest.php
@@ -24,7 +24,7 @@ class UpdateRequest extends FormRequest
         return [
             'name' => 'required|string',
             // 'country' => 'required|string',
-            'image' => 'nullable|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'image' => 'nullable|image|mimes:jpeg,png,jpg,gif,svg|max:51200',
             // 'weight' => 'required|numeric',
             // 'type' => 'required|in:1,2', 
             'category' => 'required|in:1,2',
