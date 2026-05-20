b30f1283c1039bcdbb3a53b39fd68265877b22f9
Hasanur Rashid <hr492785@gmail.com>
2026-05-20 09:43:09 +0600

    little up

--- resources/views/frontend/sister-concern/sister.blade.php
+++ resources/views/frontend/sister-concern/sister.blade.php
@@ -1,8 +1,9 @@
-@extends('frontend.layout.app')
+﻿@extends('frontend.layout.app')
 @section('title', 'Sister Concern - Mahinur Ship Recycling Limited | MSRL')
 @section('description', 'Mahinur Ship Recycling Limited (MSRL) is the sister concern of BOB Recyclers. Both specialize in green ship recycling in Sitakunda, Chittagong, Bangladesh.')
 @push('styles')
   <link rel="stylesheet" href="{{ asset('asset/css/about/about.css') }}">
+  <link rel="stylesheet" href="{{ asset('asset/css/about/sister-concern.css') }}">
 @endpush
 @section('content')
   @include('frontend.layout.nav')
@@ -17,7 +18,7 @@
   </section>
 
   <section class="container my-5 brand-text-color">
-    <h3 class="mb-4" style="font-weight: 700; color: #0d392e;">About MSRL</h3>
+    <h3 class="sister-about-heading">About MSRL</h3>
     @if($sis && $sis->about_us)
       <div style="text-align: justify; line-height: 1.8; color: #555;">
         {!! $sis->about_us !!}
@@ -27,15 +28,19 @@
 
   @if($sis && ($sis->image_1 || $sis->image_2))
     <section class="container mb-5">
-      <div class="row g-4">
+      <div class="row g-4 sister-gallery-row">
         @if($sis->image_1)
-          <div class="col-md-6">
-            <img class="img-fluid rounded-4 shadow w-100" src="{{ asset('uploads/sisterConcern/' . $sis->image_1) }}" alt="MSRL">
+          <div class="col-md-6 sister-gallery-col">
+            <div class="sister-gallery-card">
+              <img src="{{ asset('uploads/sisterConcern/' . $sis->image_1) }}" alt="MSRL ship recycling">
+            </div>
           </div>
         @endif
         @if($sis->image_2)
-          <div class="col-md-6">
-            <img class="img-fluid rounded-4 shadow w-100" src="{{ asset('uploads/sisterConcern/' . $sis->image_2) }}" alt="MSRL">
+          <div class="col-md-6 sister-gallery-col">
+            <div class="sister-gallery-card">
+              <img src="{{ asset('uploads/sisterConcern/' . $sis->image_2) }}" alt="MSRL yard operations">
+            </div>
           </div>
         @endif
       </div>
